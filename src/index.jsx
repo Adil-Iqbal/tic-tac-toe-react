e844615044275e7f296a25f44907c63719a930e8
Adil-Iqbal <aiqbal85@gmail.com>
2019-05-21 22:13:28 -0500

    Type-checking + component testing success

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,33 @@
-import { React } from 'react';
-import { ReactDOM } from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+function HelloWorld(props) {
+  const { text } = props;
+  return (
+    <h1>
+      {' '}
+      { text }
+      {' '}
+    </h1>
+  );
+}
+
+HelloWorld.propTypes = {
+  text: PropTypes.string.isRequired,
+};
 
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      text: 'Hello World!',
+    };
   }
 
   render() {
-    return (
-      <h1>Hello World</h1>
-    );
+    const { text } = this.state;
+    return (<HelloWorld text={text} />);
   }
 }
 
