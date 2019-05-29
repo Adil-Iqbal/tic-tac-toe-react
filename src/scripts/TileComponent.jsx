4d4cbfb40cdb7466939977beb7db90afb1f70248
Adil-Iqbal <aiqbal85@gmail.com>
2019-05-28 21:14:43 -0500

    add style for end-game conditions

--- src/scripts/TileComponent.jsx
+++ src/scripts/TileComponent.jsx
@@ -1,15 +1,29 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from 'react';
 import PropTypes from 'prop-types';
+import { HUMAN, COMPUTER, EMPTY } from './globalVars';
 
 function TileComponent(props) {
   const {
-    boardIndex, boardStringChar, handleClick,
+    boardIndex, boardStringChar, handleClick, signal,
   } = props;
+  function getClasses(s) {
+    const baseClasses = 'app-tile col-sm auto-mx text-center';
+    if (s === EMPTY) {
+      return `${baseClasses} empty`;
+    }
+    if (s === HUMAN) {
+      return `${baseClasses} player-wins`;
+    }
+    if (s === COMPUTER) {
+      return `${baseClasses} player-losses`;
+    }
+    return `${baseClasses} draw`;
+  }
   return (
     <div
       role="button"
-      className="app-tile col-sm auto-mx text-center"
+      className={getClasses(signal)}
       data-board_index={boardIndex}
       onClick={handleClick}
       tabIndex={-1}
@@ -23,6 +37,7 @@ TileComponent.propTypes = {
   boardIndex: PropTypes.number.isRequired,
   boardStringChar: PropTypes.string.isRequired,
   handleClick: PropTypes.func.isRequired,
+  signal: PropTypes.number.isRequired,
 };
 
 export { TileComponent as default };
