30b404e65bf5ed8e754a51e19b444b77f672a3d3
Adil-Iqbal <aiqbal85@gmail.com>
2019-05-28 21:16:49 -0500

    begin endgame logic, refractor for control flow

--- src/scripts/App.jsx
+++ src/scripts/App.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import BoardComponent from './BoardComponent';
 import UIComponent from './UIComponent';
-
-const PLAYER = 1;
-const COMPUTER = -1;
-const EMPTY = 0;
+import { HUMAN, COMPUTER, EMPTY } from './globalVars';
 
 function rollBoolean() {
   return Math.random() >= 0.5;
@@ -14,67 +11,133 @@ function rollBlunder(max, min = 0) {
   return Math.random() * (max - min) + min;
 }
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       board: [1, 0, 1, -1, 0, -1, 1, 0, 1],
+      highlight: [0, 0, 0, 0, 0, 0, 0, 0, 0],
       winConditions: [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
         [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]],
-      playersTurn: rollBoolean(),
-      playerIsX: rollBoolean(),
+      humansTurn: rollBoolean(),
+      humanIsX: rollBoolean(),
       blunderChance: rollBlunder(0.2),
       score: {
         wins: 0,
         losses: 0,
         draws: 0,
       },
+      checkBoard: false,
     };
 
     this.handleTileClick = this.handleTileClick.bind(this);
   }
 
-  componentDidUpdate() {
-    console.log('-- update --');
-    // const { playersTurn } = this.state;
-    // this.checkWin();
-    // this.checkDraw();
-    // if (!playersTurn) {
-    //   this.computersMove();
-    // }
+  componentDidMount() {
+    this.newGame();
   }
 
-  initializeGame() {
-    const newState = {
+  componentDidUpdate() {
+    const { checkBoard } = this.state;
+    if (checkBoard) {
+      this.checkWin();
+      this.checkDraw();
+    }
+  }
+
+  newGame() {
+    this.setState({
       board: [0, 0, 0, 0, 0, 0, 0, 0, 0],
-      playersTurn: App.rollBoolean(),
-      playerIsX: App.rollBoolean(),
-      blunderChance: App.rollBlunder(0.2),
-    };
-    this.setState(newState);
+      highlight: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      humansTurn: rollBoolean(),
+      humanIsX: rollBoolean(),
+      blunderChance: rollBlunder(0.2),
+      checkBoard: false,
+    });
   }
 
   checkWin() {
+    const { board, winConditions } = this.state;
+    for (let i = 0; i < winConditions.length; i += 1) {
+      let a = winConditions[i][0];
+      let b = winConditions[i][1];
+      let c = winConditions[i][2];
+      a = board[a];
+      b = board[b];
+      c = board[c];
+      if ((a === b === c) && (a !== EMPTY)) {
+        this.resolveGame(i, a);
+      }
+    }
+  }
+
+  async resolveGame(index, winner) {
+    let { score } = this.state;
+    score = Object.assign({}, score);
+    if (winner === HUMAN) {
+      score.wins += 1;
+    } else if (winner === COMPUTER) {
+      score.losses += 1;
+    }
+    this.setState({
+      highlight: this.highlightBoard(index, winner),
+      score,
+      checkBoard: false,
+    });
+    await sleep(800);
+    this.newGame();
+  }
+
+  highlightBoard(index, player) {
     const { winConditions } = this.state;
-    console.log(winConditions);
+    const [a, b, c] = winConditions[index];
+    const newDisplay = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    newDisplay[a] = player;
+    newDisplay[b] = player;
+    newDisplay[c] = player;
+    return newDisplay;
   }
 
   checkDraw() {
     const { board } = this.state;
     if (!board.includes(EMPTY)) {
-      console.log("It's a draw!");
+      this.draw();
     }
+    // else, Computer's turn begins here.
+  }
+
+  async draw() {
+    let { score } = this.state;
+    score = Object.assign({}, score);
+    score.draws += 1;
+    this.setState({
+      highlight: [2, 2, 2, 2, 2, 2, 2, 2, 2],
+      score,
+      checkBoard: false,
+    });
+    await sleep(800);
+    this.newGame();
   }
 
   handleTileClick(env) {
     const index = env.currentTarget.dataset.board_index;
-    const { board, playersTurn } = this.state;
-    if (playersTurn && board[index] === EMPTY) {
-      board[index] = PLAYER;
-      this.setState({
-        board,
-      });
+    const { board, humansTurn } = this.state;
+    if (humansTurn && board[index] === EMPTY) {
+      this.claimTile(index, HUMAN);
     }
+  }
+
+  claimTile(index, player) {
+    const { board } = this.state;
+    board[index] = player;
+    this.setState({
+      board,
+      checkBoard: true,
+    });
   }
 
   doesBlunder() {
@@ -83,10 +146,10 @@ class App extends React.Component {
   }
 
   boardToString() {
-    const { board, playerIsX } = this.state;
+    const { board, humanIsX: playerIsX } = this.state;
     let boardString = '';
     for (let i = 0; i < board.length; i += 1) {
-      if (board[i] === PLAYER) {
+      if (board[i] === HUMAN) {
         boardString += playerIsX ? 'X' : 'O';
       } else if (board[i] === COMPUTER) {
         boardString += playerIsX ? 'O' : 'X';
@@ -98,14 +161,14 @@ class App extends React.Component {
   }
 
   render() {
-    const { score, blunderChance, playersTurn } = this.state;
+    const { score, blunderChance, highlight } = this.state;
     const { wins, losses, draws } = score;
     return (
       <div className="app-main container mx-auto">
         <BoardComponent
-          playersTurn={playersTurn}
           boardString={this.boardToString()}
           handleClick={this.handleTileClick}
+          signals={highlight}
         />
         <UIComponent
           wins={wins}
